51b9c29aebeef99167bc49f85411cb43d057386b
Kyle Parrigan <kyle.parrigan@gmail.com>
2016-05-03 20:56:03 -0400

    Visual Studio solution build script

---
+++ powershell/build-solution.ps1
@@ -0,0 +1,50 @@
+﻿function Build-Solution (
+
+    [parameter(Mandatory=$true)]            
+    [ValidateNotNullOrEmpty()]  
+    [string] $solutionPath, 
+    
+    [parameter(Mandatory=$false)]            
+    [ValidateNotNullOrEmpty()]    
+    [string] $frameworkVersion = "v4.0.30319",
+
+    [parameter(Mandatory=$false)]            
+    [ValidateNotNullOrEmpty()]    
+    [string] $logFilePath = "C:\Temp\BuildLogs.txt"
+    ) 
+{
+
+    "Cleaning solution' $solutionPath'..."
+     
+    $MSBuild = $env:systemroot + "\Microsoft.NET\Framework\$frameworkVersion\MSBuild.exe";
+         
+                            
+    $args = @{            
+        FilePath = $MSBuild            
+        ArgumentList = $solutionPath, "/t:clean", ("/p:Configuration=Release"), "/v:minimal"            
+        RedirectStandardOutput = $logFilePath            
+        Wait = $true            
+        WindowStyle = "Hidden"            
+    }            
+                    
+    #Clean the solution before building
+    Start-Process @args #| Out-String -stream -width 1024 > $logFilePath    
+
+    "Finished cleaning solution' $solutionPath'..."
+
+    "Building solution' $solutionPath'..."
+
+    $args = @{            
+        FilePath = $MSBuild            
+        ArgumentList = $solutionPath, "/t:build", ("/p:Configuration=Release"), "/v:minimal"            
+        RedirectStandardOutput = $logFilePath            
+        Wait = $true            
+        WindowStyle = "Hidden"            
+    }            
+                    
+    #Build the solution
+    Start-Process @args #| Out-String -stream -width 1024 > $logFilePath   
+    
+    "Finished building solution' $solutionPath'..." 
+}
+
