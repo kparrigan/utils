420f014720b1da8bf01f545747f50dfb0d1d572b
Kyle Parrigan <kyle.parrigan@gmail.com>
2016-05-03 21:30:42 -0400

    Added param/process constructs to build-solution script

--- powershell/build-solution.ps1
+++ powershell/build-solution.ps1
@@ -1,58 +1,64 @@
-﻿function Build-Solution (
-
-    [parameter(Mandatory=$true)]            
-    [ValidateNotNullOrEmpty()]  
-    [string] $solutionPath, 
-    
-    [parameter(Mandatory=$false)]            
-    [ValidateNotNullOrEmpty()]    
-    [string] $frameworkVersion = "v4.0.30319",
-
-    [parameter(Mandatory=$false)]            
-    [ValidateNotNullOrEmpty()]    
-    [string] $logFilePath = "C:\Temp\BuildLogs.txt"
-    ) 
+﻿function Build-Solution
 {
 
-    try
-    {
+    param
+    (
+        [parameter(Mandatory=$true)]            
+        [ValidateNotNullOrEmpty()]  
+        [string] $solutionPath, 
+    
+        [parameter(Mandatory=$false)]            
+        [ValidateNotNullOrEmpty()]    
+        [string] $frameworkVersion = "v4.0.30319",
 
-        "Cleaning solution' $solutionPath'..."
+        [parameter(Mandatory=$false)]            
+        [ValidateNotNullOrEmpty()]    
+        [string] $logFilePath = "C:\Temp\BuildLogs.txt"
+    )
      
-        $MSBuild = $env:systemroot + "\Microsoft.NET\Framework\$frameworkVersion\MSBuild.exe";
+
+    process
+    {
+        try
+        {
+
+            "Cleaning solution' $solutionPath'..."
+     
+            $MSBuild = $env:systemroot + "\Microsoft.NET\Framework\$frameworkVersion\MSBuild.exe";
          
                             
-        $args = @{            
-            FilePath = $MSBuild            
-            ArgumentList = $solutionPath, "/t:clean", ("/p:Configuration=Release"), "/v:minimal"            
-            RedirectStandardOutput = $logFilePath            
-            Wait = $true            
-            WindowStyle = "Hidden"            
-        }            
+            $args = @{            
+                FilePath = $MSBuild            
+                ArgumentList = $solutionPath, "/t:clean", ("/p:Configuration=Release"), "/v:minimal"            
+                RedirectStandardOutput = $logFilePath            
+                Wait = $true            
+                WindowStyle = "Hidden"            
+            }            
                     
-        #Clean the solution before building
-        Start-Process @args #| Out-String -stream -width 1024 > $logFilePath    
+            #Clean the solution before building
+            Start-Process @args #| Out-String -stream -width 1024 > $logFilePath    
 
-        "Finished cleaning solution' $solutionPath'..."
+            "Finished cleaning solution' $solutionPath'..."
 
-        "Building solution' $solutionPath'..."
+            "Building solution' $solutionPath'..."
 
-        $args = @{            
-            FilePath = $MSBuild            
-            ArgumentList = $solutionPath, "/t:build", ("/p:Configuration=Release"), "/v:minimal"            
-            RedirectStandardOutput = $logFilePath            
-            Wait = $true            
-            WindowStyle = "Hidden"            
-        }            
+            $args = @{            
+                FilePath = $MSBuild            
+                ArgumentList = $solutionPath, "/t:build", ("/p:Configuration=Release"), "/v:minimal"            
+                RedirectStandardOutput = $logFilePath            
+                Wait = $true            
+                WindowStyle = "Hidden"            
+            }            
                     
-        #Build the solution
-        Start-Process @args #| Out-String -stream -width 1024 > $logFilePath   
+            #Build the solution
+            Start-Process @args #| Out-String -stream -width 1024 > $logFilePath   
     
-        "Finished building solution' $solutionPath'..." 
-    }
-    catch
-    {
-        "Unexpected error while building solution' $solutionPath'..."
+            "Finished building solution' $solutionPath'..." 
+        }
+        catch
+        {
+            "Unexpected error while building solution' $solutionPath'..."
+        }
     }
 }
 
