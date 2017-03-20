6e38b177ee45e2a88a4eb7b5e29be4c7d24d099e
Kyle Parrigan <Kyle Parrigan>
2017-03-20 19:32:51 -0400

    Adding powershell file system utility function.

---
+++ powershell/get-all-server-items.ps1
@@ -0,0 +1,56 @@
+﻿function Get-All-Server-Items
+{
+    param
+    (
+        [parameter(Mandatory=$true)]            
+        [ValidateNotNullOrEmpty()]  
+        [string] $filter, 
+
+        [parameter(Mandatory=$false)]            
+        [ValidateNotNullOrEmpty()]  
+        [string] $manifestFile, 
+
+        [parameter(Mandatory=$false)]  
+        [switch] $delete
+    )
+
+    process
+    {
+        try
+        {
+            $childItems = New-Object System.Collections.Generic.List[System.String]
+            $drives = Get-PSDrive -PSProvider 'FileSystem'
+
+            ForEach ($drive in $drives) {
+                $root = $drive.Root
+                "Searching: " + $root
+
+                $children = Get-ChildItem -Path $root -Include $filter -Recurse;
+                ForEach($child in $children) {
+                    $child.FullName
+                    $childItems.Add($child.FullName)
+                }
+
+                if ($manifestFile)
+                {
+                    $childItems | Format-Table Fullname | Out-File $manifestFile
+                }
+
+                if ($delete)
+                {
+                    "Deleting Files..."
+                    ForEach ($childItem in $childItems)
+                    {
+                        Remove-Item $childItem
+                    }
+                }
+            }
+
+            "Processing complete..."
+        }
+        catch
+        {
+            "Unexpected error while processing..."
+        }
+    }
+}
